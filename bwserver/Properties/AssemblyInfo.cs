4110a0081e64e30b3d9f36bffec995a99ed3b358
MBA <148908741+mba0@users.noreply.github.com>
2024-03-31 21:06:07 +0200

    Add files via upload

--- bwserver/Properties/AssemblyInfo.cs
+++ bwserver/Properties/AssemblyInfo.cs
@@ -1,1 +1,17 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
+[assembly: AssemblyTitle("Breaworlds.Server")]
+[assembly: AssemblyDescription("Breaworlds server")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Quu98")]
+[assembly: AssemblyProduct("Breaworlds.Server")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("efcd47c6-63e7-4975-b279-84bd9a36dc62")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
